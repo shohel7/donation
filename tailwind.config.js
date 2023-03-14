557f849d166900ae66f26763c06fc3fc57d11ee3
shohel-ranaC <akashshohel422@gmail.com>
2023-03-14 08:44:52 -0700

    donation main page is completed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#FFC265",
+        secondaryColor: "#1DA599",
+
+        // ...
+      },
+    },
+  },
+  plugins: [],
+};
