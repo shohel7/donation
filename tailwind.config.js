7d5dab4eadc3f67b75d97a21d3026d03e568fd55
shohel-ranaC <akashshohel422@gmail.com>
2023-03-22 06:18:03 -0700

    typography fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      Inter: ['"Inter"', "sans-serif"],
+    },
     extend: {
       colors: {
         primaryColor: "#FFC265",
